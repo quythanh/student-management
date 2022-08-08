e6ac702ae783d7eb999ea270539cc9d22bfa1623
Quy Thanh <quythanh2205@gmail.com>
2022-08-08 21:26:56 +0700

    rewrite code

--- functions.cpp
+++ functions.cpp
@@ -1,60 +1,47 @@
 #include "functions.h"
 
 void start() {
-    Menu();
-}
-
-void cls() {
     system("cls");
+    Menu();
 }
 
 void pause() {
     char tmp;
     cout << endl << "Nhan Enter de tiep tuc...";
-    cin.ignore();
     getchar();
-    Menu();
+    system("cls");
 }
 
 void Menu() {
-    cls();
-
-    int option;
-
-    cout << "***-------------     QUAN LY DANH SACH SINH VIEN     --------------***" << endl;
-    cout << "1. Tim kiem thong tin sinh vien theo MSSV" << endl;
-    cout << "2. Them sinh vien moi" << endl;
-    cout << "3. Xoa sinh vien theo MSSV" << endl;
-    cout << "4. In danh sach sinh vien" << endl;
-    cout << "5. Thoat" << endl;
-
-    HandleOption();
-}
-
-void HandleOption() {
-    int option;
     char c;
-    string ID;
-    Node *pStudentsListHead = nullptr;
+    int option;
+    ListStudent list;
+    list.ReadData();
 
     while (true) {
+        cout << "***-------------     QUAN LY DANH SACH SINH VIEN     --------------***" << endl;
+        cout << "1. Tim kiem thong tin sinh vien theo MSSV" << endl;
+        cout << "2. Them sinh vien moi" << endl;
+        cout << "3. Xoa sinh vien theo MSSV" << endl;
+        cout << "4. In danh sach sinh vien" << endl;
+        cout << "5. Thoat" << endl;
         cout << "Nhap lua chon cua ban:\t";
         cin >> option;
         cin.ignore();
 
         switch (option) {
             case 1:
-                FindStudent();
+                FindStudent(list);
                 pause();
                 break;
             
             case 2:
-                InsertStudent();
+                InsertStudent(list);
                 pause();
                 break;
             
             case 3:
-                DeleteStudent();
+                DeleteStudent(list);
                 pause();
                 break;
             
@@ -65,9 +52,10 @@ void HandleOption() {
                 cout << "b. Theo diem trung binh giam dan" << endl;
                 cout << "c. Theo cac sinh vien Dau, Rot" << endl;
                 cout << "Nhap lua chon cua ban:\t";
-                while (cin >> c && (c < 'a' || c > 'e') && (c < 'A' || c > 'C'))
+                while (cin >> c && (c < 'a' || c > 'c') && (c < 'A' || c > 'C'))
                     cout << "Loi! Vui long nhap lua chon hop le:\t";
-                HandlePrintStudents(c);
+                cin.ignore();
+                HandlePrint(list, c);
                 pause();
                 break;
             
@@ -82,333 +70,83 @@ void HandleOption() {
     }
 }
 
-bool ReadFile(Node *&pStudentsListHead) {
-    ifstream f("students.txt");
-
-    if (pStudentsListHead != nullptr)
-        return false;
-    else
-        while (!f.eof()) {
-            Student student;
-
-            getline(f, student.ID, '-');
-            getline(f, student.lastName, '-');
-            getline(f, student.firstName, '-');
-            f >> student.midtermMark;
-            f.ignore();
-            f >> student.finalMark;
-            f.ignore();
-
-            if (student.ID != "")
-                InsertEnd(pStudentsListHead, student);
-        }
-    
-    f.close();
-    return true;
-}
-
-void WriteFile(Node *pStudentsListHead) {
-    ofstream f("students.txt");
-
-    if (pStudentsListHead == nullptr)
-        f << "";
-
-    while (pStudentsListHead != nullptr) {
-        Student student = pStudentsListHead->student;
-        f << student.ID << "-" << student.lastName << "-" << student.firstName << "-" << student.midtermMark << "-" << student.finalMark << endl;
-        pStudentsListHead = pStudentsListHead->pNext;
-    }
-
-    f.close();
-}
-
-void HandlePrintStudents(char option, string ID) {
-    Node *pStudentsListHead = nullptr;
-    Node *pPassedStudents = nullptr;
-    Node *pFailedStudents = nullptr;
-    ReadFile(pStudentsListHead);
-
+void HandlePrint(ListStudent list, char option) {
+    system("cls");
+    ListStudent newList;
     switch (option) {
         case 'a':
-            SortStudentsByName(pStudentsListHead);
-            
-            cls();
             cout << "***-------------     DANH SACH SINH VIEN SAP XEP THEO TEN    --------------***" << endl;
-            PrintStudents(pStudentsListHead);
-            
+            list.SortByName().PrintStudent();
             break;
-        
+
         case 'b':
-            SortStudentsByMarks(pStudentsListHead);
-            
-            cls();
             cout << "***-------------     DANH SACH SINH VIEN SAP XEP THEO DIEM    --------------***" << endl;
-            PrintStudents(pStudentsListHead);
-            
+            list.SortByMarks().PrintStudent();
             break;
-        
+
         case 'c':
-            pPassedStudents = SeperatePassedStudents(pStudentsListHead);
-            pFailedStudents = SeperateFailedStudents(pStudentsListHead);
+            // Danh sach sinh vien dau
+            newList = list.GetPassedStudents();
+            if (!newList.isEmpty()) {
+                cout << "***-------------     DANH SACH SINH VIEN DAU    --------------***" << endl;
+                newList.PrintStudent();
+            }
 
-            cls();
-            cout << "***-------------     DANH SACH SINH VIEN DAU    --------------***" << endl;
-            PrintStudents(pPassedStudents);
-            
-            cout << "***-------------     DANH SACH SINH VIEN ROT    --------------***" << endl;
-            PrintStudents(pFailedStudents);
-
-            Delete(pPassedStudents);
-            Delete(pFailedStudents);
+            // Danh sach sinh vien rot
+            newList = list.GetFailedStudents();
+            if (!newList.isEmpty()) {
+                cout << "***-------------     DANH SACH SINH VIEN ROT    --------------***" << endl;
+                newList.PrintStudent();
+            }
             break;
-        
+
         default:
-            cls();
             cout << "***-------------     DANH SACH SINH VIEN    --------------***" << endl;
-            PrintStudents(pStudentsListHead);
+            list.PrintStudent();
             break;
     }
-
-    
-    Delete(pStudentsListHead);
 }
 
-void PrintStudents(Node *pStudentsListHead) {    
-    cout << "-------------------------------------------------------------------------------------------------" << endl;
-    cout << "|     MSSV     |          Ho          |     Ten     | Diem mon 1 | Diem mon 2 | Diem trung binh |" << endl;
-    cout << "-------------------------------------------------------------------------------------------------" << endl;
 
-    while (pStudentsListHead != nullptr) {
-        Student s = pStudentsListHead->student;
-        
-        cout << "|" << setw(14) << Format(s.ID, 14).Center();
-        cout << "|" << setw(22) << Format(s.lastName, 22).Left(3);
-        cout << "|" << setw(13) << Format(s.firstName, 13).Left(3);
-        cout << "|" << setw(12) << Format(s.midtermMark, 12).Right(3);
-        cout << "|" << setw(12) << Format(s.finalMark, 12).Right(3);
-        cout << "|" << setw(17) << Format((s.midtermMark + s.finalMark)/2, 17).Right(3);
-        cout << "|" << endl;
-        cout << "-------------------------------------------------------------------------------------------------" << endl;
-
-        pStudentsListHead = pStudentsListHead->pNext;
-    }
-    cout << endl;
-}
-
-// Linked List Functions
-void InsertBeginning(Node *&pStudentsListHead, Student student) {
-    if (pStudentsListHead == nullptr) {
-        pStudentsListHead = new Node;
-        pStudentsListHead->student = student;
-        pStudentsListHead->pNext = nullptr;
-    } else {
-        Node *pNew = new Node;
-        pNew->student = student;
-        pNew->pNext = pStudentsListHead;
-        
-        pStudentsListHead = pNew;
-    }
-}
-
-void InsertEnd(Node *&pStudentsListHead, Student student) {
-    Node *pCur = pStudentsListHead;
-
-    if (pStudentsListHead == nullptr) {
-        pCur = new Node;
-        pCur->student = student;
-        pCur->pNext = nullptr;
-
-        pStudentsListHead = pCur;
-    } else {
-        while (pCur->pNext != nullptr)
-            pCur = pCur->pNext;
-
-        Node *pNew = new Node;
-        pNew->student = student;
-        pNew->pNext = nullptr;
-        
-        pCur->pNext = pNew;
-    }
-}
-
-Node *FindNode(Node *pStudentsListHead, string ID) {
-    while (pStudentsListHead != nullptr && pStudentsListHead->student.ID != ID)
-        pStudentsListHead = pStudentsListHead->pNext;
-    
-    Node *pNew = nullptr;
-    if (pStudentsListHead != nullptr) {
-        pNew = new Node;
-        pNew->student = pStudentsListHead->student;
-        pNew->pNext = nullptr;
-    }
-
-    return pNew;
-}
-
-bool swapNode(Node *&pStudentsListHead, Student A, Student B) {
-    Node *pStudentA = pStudentsListHead;
-    while (pStudentA != nullptr && pStudentA->student.ID != A.ID)
-        pStudentA = pStudentA->pNext;
-    
-    Node *pStudentB = pStudentsListHead;
-    while (pStudentB != nullptr && pStudentB->student.ID != B.ID)
-        pStudentB = pStudentB->pNext;
-
-    if (pStudentA == nullptr || pStudentB == nullptr)
-        return false;
-
-    pStudentA->student = B;
-    pStudentB->student = A;
-    
-    return true;
-}
-
-bool DeleteNode(Node *&pStudentsListHead, string ID) {
-    Node *pCur = pStudentsListHead;
-    Node *pPrev;
-    while (pCur != nullptr && pCur->student.ID != ID) {
-        pPrev = pCur;
-        pCur = pCur->pNext;
-    }
-    
-    if (pCur == nullptr)
-        return false;
-
-    pPrev->pNext = pCur->pNext;
-    delete pCur;
-    return true;
-}
-
-bool Delete(Node *&pStudentsListHead) {
-    if (pStudentsListHead == nullptr)
-        return false;
-
-    while (pStudentsListHead != nullptr) {
-        Node *pCur = pStudentsListHead;
-        pStudentsListHead = pStudentsListHead->pNext;
-        delete pCur;
-    }
-    return true;
-}
-
-// Student's Functions
-void FindStudent() {
+// Menu Handle Functions
+void FindStudent(ListStudent list) {
     string ID;
-    Node *pStudentsListHead = nullptr;
-    Node *pStudent = nullptr;
-
     cout << "Nhap ma sinh vien muon tim:\t";
     getline(cin, ID);
     
-    ReadFile(pStudentsListHead);
+    Node *student = list.SearchStudent(ID);
     
-    pStudent = FindNode(pStudentsListHead, ID);
-    if (pStudent != nullptr)
-        PrintStudents(pStudent);
+    if (student)
+        list.PrintStudent(student, 1);
     else
         cout << "Khong ton tai sinh vien co ma so " << ID << endl;
-    delete pStudent;
-    
-    Delete(pStudentsListHead);
 }
 
-void InsertStudent() {
-    Node *pStudentsListHead = nullptr;
+void InsertStudent(ListStudent &list) {
     Student student;
-    
-    ReadFile(pStudentsListHead);
-
     student.getInfo();
-    Node *pStudent = FindNode(pStudentsListHead, student.ID);
-    if (pStudent == nullptr) {
-        InsertEnd(pStudentsListHead, student);
-        WriteFile(pStudentsListHead);
-    }
+
+    Node *pStudent = list.SearchStudent(student.ID);
+    if (pStudent == nullptr)
+        list.InsertEnd(student, 1);
     else
         cout << "Loi! Ma sinh vien da ton tai." << endl;
-
-    delete pStudent;
-    Delete(pStudentsListHead);
 }
 
-void SortStudentsByName(Node *&pStudentsListHead) {
-    Node *pCur = pStudentsListHead;
-
-    while (pCur != nullptr) {
-        Node *pNext = pCur->pNext;
-        
-        while (pNext != nullptr) {
-            if (pCur->student.firstName > pNext->student.firstName)
-                swapNode(pStudentsListHead, pCur->student, pNext->student);
-            pNext = pNext->pNext;
-        }
-        pCur = pCur->pNext;
-    }
-}
-
-void SortStudentsByMarks(Node *&pStudentsListHead) {
-    Node *pCur = pStudentsListHead;
-
-    while (pCur != nullptr) {
-        double curMark = (pCur->student.midtermMark + pCur->student.finalMark)/2;
-        Node *pNext = pCur->pNext;
-        
-        while (pNext != nullptr) {
-            double nextMark = (pNext->student.midtermMark + pNext->student.finalMark)/2;
-            if (curMark < nextMark)
-                swapNode(pStudentsListHead, pCur->student, pNext->student);
-
-            pNext = pNext->pNext;
-        }
-        pCur = pCur->pNext;
-    }
-}
-
-Node *SeperatePassedStudents(Node *pStudentsListHead) {
-    Node *pPassedStudents = nullptr;
-
-    while (pStudentsListHead != nullptr) {
-        double totalMark = (pStudentsListHead->student.midtermMark + pStudentsListHead->student.finalMark)/2;
-        if (totalMark >= 5)
-            InsertEnd(pPassedStudents, pStudentsListHead->student);
-        pStudentsListHead = pStudentsListHead->pNext;
-    }
-
-    return pPassedStudents;
-}
-
-Node *SeperateFailedStudents(Node *pStudentsListHead) {
-    Node *pFailedStudents = nullptr;
-
-    while (pStudentsListHead != nullptr) {
-        double totalMark = (pStudentsListHead->student.midtermMark + pStudentsListHead->student.finalMark)/2;
-        if (totalMark < 5)
-            InsertEnd(pFailedStudents, pStudentsListHead->student);
-        pStudentsListHead = pStudentsListHead->pNext;
-    }
-
-    return pFailedStudents;
-}
-
-void DeleteStudent() {
+void DeleteStudent(ListStudent &list) {
     char c;
     string ID;
-    Node *pStudentsListHead = nullptr;
 
     cout << "Nhap ma sinh vien muon xoa:\t";
     getline(cin, ID);
-    ReadFile(pStudentsListHead);
     
-    if (DeleteNode(pStudentsListHead, ID)) {
-        WriteFile(pStudentsListHead);
+    if (list.DeleteStudent(ID)) {
         cout << "Da xoa thanh cong sinh vien co ma so " << ID << endl;
         cout << "Ban co muon xem danh sach sinh vien khong (y/n)?\t";
         cin >> c;
         cin.ignore();
         if (c == 'y' || c == 'Y')
-            HandlePrintStudents('d');
+            list.PrintStudent();
     } else
         cout << "Ma sinh vien khong ton tai!" << endl;
-    
-    Delete(pStudentsListHead);
 }
--- functions.h
+++ functions.h
@@ -7,6 +7,66 @@
 using namespace std;
 
 // ========================= STRUCTS SECTION =========================
+class Format {
+private:
+    string str;
+    int width;
+    string padding_left;
+    string padding_right;
+
+public:
+    Format(string s, int w) {
+        str = s;
+        width = w;
+        padding_left = padding_right = "";
+    }
+
+    Format(double d, int w) {
+        str = to_string(d);
+        if (str[2] == '0' && str[3] == '0')
+            str = str.substr(0, str.find("."));
+        else if (str[3] == '0')
+            str = str.substr(0, str.find(".")+2);
+        else
+            str = str.substr(0, str.find(".")+3);
+        width = w;
+        padding_left = padding_right = "";
+    }
+
+    string Center() {
+        int pw = width-str.length();
+        
+        for (int i = 0; i < pw/2; i++) {
+            padding_left += " ";
+            padding_right += " ";
+        }
+        
+        return padding_left + str + padding_right;
+    }
+
+    string Left(int p = 1) {
+        int pw = width-str.length()-p;
+        
+        for (int i = 0; i < pw; i++)
+            padding_right += " ";
+        for (int i = 0; i < p; i++)
+            padding_left += " ";
+        
+        return padding_left + str + padding_right;
+    }
+
+    string Right(int p = 1) {
+        int pw = width-str.length()-p;
+        
+        for (int i = 0; i < pw; i++)
+            padding_left += " ";
+        for (int i = 0; i < p; i++)
+            padding_right += " ";
+        
+        return padding_left + str + padding_right;
+    }
+};
+
 struct Student {
     string ID; // MSSV
     string firstName; // Ten
@@ -33,108 +93,260 @@ struct Student {
         cin >> finalMark;
         cin.ignore();
     }
+
+    void Print() {
+        cout << "|" << setw(14) << Format(ID, 14).Center();
+        cout << "|" << setw(22) << Format(lastName, 22).Left(3);
+        cout << "|" << setw(13) << Format(firstName, 13).Left(3);
+        cout << "|" << setw(12) << Format(midtermMark, 12).Right(3);
+        cout << "|" << setw(12) << Format(finalMark, 12).Right(3);
+        cout << "|" << setw(17) << Format((midtermMark + finalMark)/2, 17).Right(3);
+        cout << "|" << endl;
+        cout << "-------------------------------------------------------------------------------------------------" << endl;
+    }
 };
 
 struct Node {
     Student student;
-    Node *pNext;
+    Node *next;
+    
+    Node() = default;
+    Node(Student student, Node *next = nullptr) {
+        this->student = student;
+        this->next = next;
+    }
 };
 
-class Format {
+class ListStudent {
 private:
-    string str;
-    int width;
+    Node *StudentHead;
 
 public:
-    Format(string s, int w) {
-        str = s;
-        width = w;
+    ListStudent() {
+        StudentHead = nullptr;   
     }
 
-    Format(double d, int w) {
-        str = to_string(d);
-        if (str[2] == '0' && str[3] == '0')
-            str = str.substr(0, str.find("."));
-        else if (str[3] == '0')
-            str = str.substr(0, str.find(".")+2);
-        else
-            str = str.substr(0, str.find(".")+3);
-        width = w;
+    ~ListStudent() {
+        while (StudentHead != nullptr) {
+            Node *pCur = StudentHead;
+            StudentHead = StudentHead->next;
+            delete pCur;
+        }
     }
 
-    string Center() {
-        string padding_left = "";
-        string padding_right = "";
-        int pw = width-str.length();
-        
-        for (int i = 0; i < pw/2; i++) {
-            padding_left += " ";
-            padding_right += " ";
+    Node *GetStudentHead() {
+        return StudentHead;
+    }
+
+    void ReadData() {
+        ifstream f("students.txt");
+        while (!f.eof()) {
+            Student student;
+
+            getline(f, student.ID, '-');
+            getline(f, student.lastName, '-');
+            getline(f, student.firstName, '-');
+            f >> student.midtermMark;
+            f.ignore();
+            f >> student.finalMark;
+            f.ignore();
+
+            if (student.ID != "")
+                InsertEnd(student);
+        }  
+        f.close();
+    }
+
+    bool isEmpty() {
+        return StudentHead ? false : true;
+    }
+
+    void WriteStudentsToFile() {
+        ofstream f("students.txt");
+        Node *pCur = StudentHead;
+
+        if (pCur == nullptr)
+            f << "";
+
+        while (pCur != nullptr) {
+            Student student = pCur->student;
+            f << student.ID << "-" << student.lastName << "-" << student.firstName << "-" << student.midtermMark << "-" << student.finalMark << endl;
+            pCur = pCur->next;
+        }
+
+        f.close();
+    }
+
+    void InsertBeginning(Student newStudent) {
+        StudentHead = new Node(newStudent, StudentHead);
+    }
+
+    void InsertEnd(Student newStudent, int write = 0) {
+        if (StudentHead == nullptr)
+            StudentHead = new Node(newStudent);
+        else {
+            Node *pCur = StudentHead;
+            while (pCur->next != nullptr)
+                pCur = pCur->next;
+            pCur->next = new Node(newStudent);
+        }
+
+        if (write)
+            WriteStudentsToFile();
+    }
+
+    Node *SearchStudent(string ID) {
+        Node *pCur = StudentHead;
+        while (pCur != nullptr && pCur->student.ID != ID)
+            pCur = pCur->next;       
+        return pCur;
+    }
+
+    bool SwapStudents(Student A, Student B) {
+        Node *pStudentA = SearchStudent(A.ID);
+        Node *pStudentB = SearchStudent(B.ID);
+
+        if (pStudentA == nullptr || pStudentB == nullptr)
+            return false;
+
+        pStudentA->student = B;
+        pStudentB->student = A;
+        return true;
+    }
+
+    bool DeleteStudent(string ID) {
+        Node *pCur = StudentHead;
+        Node *pPrev;
+        while (pCur != nullptr && pCur->student.ID != ID) {
+            pPrev = pCur;
+            pCur = pCur->next;
         }
         
-        return padding_left + str + padding_right;
+        if (pCur == nullptr)
+            return false;
+
+        pPrev->next = pCur->next;
+        delete pCur;
+        WriteStudentsToFile();
+        
+        return true;
     }
 
-    string Left(int p = 1) {
-        string padding_left = "";
-        string padding_right = "";
-        int pw = width-str.length()-p;
-        
-        for (int i = 0; i < pw; i++)
-            padding_right += " ";
-        for (int i = 0; i < p; i++)
-            padding_left += " ";
-        
-        return padding_left + str + padding_right;
+    void PrintStudent(Node *student = nullptr, int amount = 0) {
+        cout << "-------------------------------------------------------------------------------------------------" << endl;
+        cout << "|     MSSV     |          Ho          |     Ten     | Diem mon 1 | Diem mon 2 | Diem trung binh |" << endl;
+        cout << "-------------------------------------------------------------------------------------------------" << endl;
+
+        Node *pCur = student ? student : StudentHead;
+        if (!amount)
+            while (pCur != nullptr) {
+                pCur->student.Print();
+                pCur = pCur->next;
+            }
+
+        while (amount-- && pCur != nullptr) {
+            pCur->student.Print();
+            pCur = pCur->next;
+        }
+
+        cout << endl;
     }
 
-    string Right(int p = 1) {
-        string padding_left = "";
-        string padding_right = "";
-        int pw = width-str.length()-p;
+    ListStudent GetPassedStudents() {
+        Node *student = StudentHead;
+        ListStudent ListPassedStudents;
+
+        while (student != nullptr) {
+            double totalMark = (student->student.midtermMark + student->student.finalMark)/2;
+            if (totalMark >= 5)
+                ListPassedStudents.InsertEnd(student->student);
+            student = student->next;
+        }
+
+        return ListPassedStudents;
+    }
+
+    ListStudent GetFailedStudents() {
+        Node *student = StudentHead;
+        ListStudent ListFailedStudents;
+
+        while (student != nullptr) {
+            double totalMark = (student->student.midtermMark + student->student.finalMark)/2;
+            if (totalMark < 5)
+                ListFailedStudents.InsertEnd(student->student);
+            student = student->next;
+        }
+
+        return ListFailedStudents;
+    }
+
+    ListStudent SortByName() {
+        ListStudent newList;
+        newList.ReadData();
         
-        for (int i = 0; i < pw; i++)
-            padding_left += " ";
-        for (int i = 0; i < p; i++)
-            padding_right += " ";
-        
-        return padding_left + str + padding_right;
+        Node *pCur = newList.GetStudentHead();
+
+        while (pCur != nullptr) {
+            Node *next = pCur->next;
+            
+            while (next != nullptr) {
+                if (pCur->student.firstName > next->student.firstName)
+                    newList.SwapStudents(pCur->student, next->student);
+                next = next->next;
+            }
+            pCur = pCur->next;
+        }
+
+        return newList;
+    }
+
+    ListStudent SortByMarks() {
+        ListStudent newList;
+        newList.ReadData();
+
+        Node *pCur = newList.GetStudentHead();
+
+        while (pCur != nullptr) {
+            double curMark = (pCur->student.midtermMark + pCur->student.finalMark)/2;
+            Node *next = pCur->next;
+            
+            while (next != nullptr) {
+                double nextMark = (next->student.midtermMark + next->student.finalMark)/2;
+                if (curMark < nextMark)
+                    newList.SwapStudents(pCur->student, next->student);
+                next = next->next;
+            }
+            pCur = pCur->next;
+        }
+
+        return newList;
     }
 };
 
 // ========================= FUNCTIONS SECTION =========================
 
-// General Functions
 void start();
+
+/* Tạm dừng chương trình để người dùng xem danh sách được in */
 void pause();
-void cls();
+
+/* In menu */
 void Menu();
-void HandleOption();
 
-// File
-bool ReadFile(Node *&pStudentsListHead);
-void WriteFile(Node *pStudentsListHead);
+/*
+    Hàm nhận vào 2 tham số: list, option
+        ListStudent list: Danh sách sinh viên
+        char option: Các lựa chọn để in sinh viên
+            - Các giá trị:
+                a: Tên đã được sắp xếp theo thứ tự tăng dần
+                b: Điểm đã sắp xếp theo thứ tự tăng dần
+                c: Lọc ra sinh viên đậu, rớt
+                default: In theo thứ tự trong file
+*/
+void HandlePrint(ListStudent list, char option);
 
-// Print & Format
-void HandlePrintStudents(char option, string ID = "") ;
-void PrintStudents(Node *pStudentsListHead);
 
-// Linked List Functions
-void InsertBeginning(Node *&pStudentsListHead, Student student);
-void InsertEnd(Node *&pStudentsListHead, Student student);
-
-Node *FindNode(Node *pStudentsListHead, string ID);
-
-bool swapNode(Node *&pStudentsListHead, Student A, Student B);
-
-bool DeleteNode(Node *&pStudentsListHead, string ID);
-bool Delete(Node *&pStudentsListHead);
-
-// Student's Functions
-void FindStudent();
-void InsertStudent();
-void SortStudentsByName(Node *&pStudentsListHead);
-void SortStudentsByMarks(Node *&pStudentsListHead);
-Node *SeperatePassedStudents(Node *pStudentsListHead);
-Node *SeperateFailedStudents(Node *pStudentsListHead);
-void DeleteStudent();
+// Menu Handle Functions
+void FindStudent(ListStudent list);
+void InsertStudent(ListStudent &list);
+void DeleteStudent(ListStudent &list);
